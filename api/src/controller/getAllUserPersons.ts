04559aeeb771e22f6267c297c7800f00c0407c95
Alexis Weber <Alexis.Weber@mailfence.com>
2023-08-09 14:12:07 -0300

    upgrade 09-08-23 14:12hs

--- api/src/controller/getAllUserPersons.ts
+++ api/src/controller/getAllUserPersons.ts
@@ -1,23 +1,18 @@
 import { Request, Response } from "express";
 import { UserPerson } from "../../db";
 
-const getAllUserPersons = async (req: Request, res: Response)=>{
-    try {
+const getAllUserPersons = async (req: Request, res: Response) => {
+  try {
+    const users = await UserPerson.findAll();
 
-        const users = await UserPerson.findAll();
-
-        if(!users){
-            return res.status(404).send({ message: 'Users not found.' })
-        };
-
-        console.log(users);
-        
-        return res.status(200).send(users);
-
-    } catch (error) {
-
-        return res.status(500).send({ message: 'Internal Server Error.' });
+    if (!users) {
+      return res.status(404).send({ message: "Users not found." });
     }
+
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send({ message: "Internal Server Error." });
+  }
 };
 
 export default getAllUserPersons;
