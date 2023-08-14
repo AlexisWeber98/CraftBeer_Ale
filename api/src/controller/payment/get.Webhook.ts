538ca429a109c15ca89ad86f887a418b8311dc42
Alexis Weber <Alexis.Weber@Mailfence.com>
2023-08-14 20:28:27 -0300

    agregar logs para revisar respuesta de succes

--- api/src/controller/payment/get.Webhook.ts
+++ api/src/controller/payment/get.Webhook.ts
@@ -3,6 +3,12 @@ import { Request, Response } from "express"
 const recibeWebHook = (req: Request, res:Response) => {
 const {id, type}  = req.query
 
+console.log("lo que sigue es es la respuesta luego de la efectizacion de la orden de pago");
+
+
+console.log(req.query);
+
+res.send(req.query)
 
 }
 
