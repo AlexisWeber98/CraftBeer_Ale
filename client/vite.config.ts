e5d2770575f4255b39052f198f22829ae40a6cce
Alexis Weber <Alexis.weber@mailfence.com>
2024-11-01 14:53:15 -0300

    add headers proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,7 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
@@ -12,5 +10,23 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
-  base: "./", // Ajusta esto según la subcarpeta de tu dominio
+  base: "./", 
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://sh1cc4n7-3000.brs.devtunnels.ms',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, _req, _res) => {
+            proxyReq.setHeader('Access-Control-Allow-Origin', '*');
+            proxyReq.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS');
+            proxyReq.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            proxyReq.setHeader('Access-Control-Allow-Credentials', 'true');
+          });
+        },
+      },
+    },
+  },
 });
+
