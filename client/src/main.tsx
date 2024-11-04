2d42290b72ee29ea276ed4781da9473fbb66115b
Alexis Weber <Alexis.weber@mailfence.com>
2024-11-04 12:45:22 -0300

    update axios connfig (withCredentials = false)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 // axios.defaults.baseURL = 'http://localhost:3001'
 axios.defaults.baseURL = 'https://craftbeer-api.fly.dev/'
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
